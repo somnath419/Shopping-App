a4112f830f022687f7606f6b3eb55206bd8d8c22
SOMNATH <somnath419@gmail.com>
2017-11-21 22:08:46 +0530

    pushing

--- app/src/main/java/com/example/somnath/kart/AccountFragment.java
+++ app/src/main/java/com/example/somnath/kart/AccountFragment.java
@@ -29,7 +29,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
